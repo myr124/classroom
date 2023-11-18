7f5c854cb5728844caf99041641c43d5dd63ed66
Eric George <ericgeorge@Erics-MacBook-Air.local>
2023-11-17 20:30:19 -0500

    button and basic to speech to text functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+"use client";
+
 import Translation from "@/components/translation";
-import fs from "fs";
 
 export default function Home() {
-  const file = fs.readFileSync("public/YourAudioFIle.wav");
   return (
     <div>
       <Translation />
--- components/translation.tsx
+++ components/translation.tsx
@@ -7,41 +7,54 @@ const speechConfig = mcsSDK.SpeechConfig.fromSubscription(
   process.env.NEXT_PUBLIC_SPEECH_REGION
 );
 
-speechConfig.speechRecognitionLanguage = "en-US";
+speechConfig.speechRecognitionLanguage = "ml";
 
 const Translation = () => {
-  const [text, setText] = useState("");
-  useEffect(() => {
-    const audioConfig = mcsSDK.AudioConfig.fromDefaultMicrophoneInput();
-    const speechRecognizer = new mcsSDK.SpeechRecognizer(
-      speechConfig,
-      audioConfig
-    );
-    speechRecognizer.recognizeOnceAsync(async (result) => {
-      switch (result.reason) {
-        case mcsSDK.ResultReason.RecognizedSpeech:
-          console.log(`RECOGNIZED: Text=${result.text}`);
-          break;
-        case mcsSDK.ResultReason.NoMatch:
-          console.log("NOMATCH: Speech could not be recognized.");
-          break;
-        case mcsSDK.ResultReason.Canceled:
-          const cancellation = mcsSDK.CancellationDetails.fromResult(result);
-          console.log(`CANCELED: Reason=${cancellation.reason}`);
-
-          if (cancellation.reason == mcsSDK.CancellationReason.Error) {
-            console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
-            console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
-            console.log(
-              "CANCELED: Did you set the speech resource key and region values?"
-            );
-          }
-          break;
-      }
-      speechRecognizer.close();
-    });
-  }, []);
-  return <div>{text}</div>;
+  const [text, setText] = useState("test");
+  const [micOn, setMicOn] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          const audioConfig = mcsSDK.AudioConfig.fromDefaultMicrophoneInput();
+          const speechRecognizer = new mcsSDK.SpeechRecognizer(
+            speechConfig,
+            audioConfig
+          );
+          speechRecognizer.recognizeOnceAsync(async (result) => {
+            console.log("Starting");
+            switch (result.reason) {
+              case mcsSDK.ResultReason.RecognizedSpeech:
+                console.log(`RECOGNIZED: Text=${result.text}`);
+                setText(result.text);
+                break;
+              case mcsSDK.ResultReason.NoMatch:
+                console.log("NOMATCH: Speech could not be recognized.");
+                break;
+              case mcsSDK.ResultReason.Canceled:
+                const cancellation =
+                  mcsSDK.CancellationDetails.fromResult(result);
+                console.log(`CANCELED: Reason=${cancellation.reason}`);
+                if (cancellation.reason == mcsSDK.CancellationReason.Error) {
+                  console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
+                  console.log(
+                    `CANCELED: ErrorDetails=${cancellation.errorDetails}`
+                  );
+                  console.log(
+                    "CANCELED: Did you set the speech resource key and region values?"
+                  );
+                }
+                break;
+            }
+            speechRecognizer.close();
+          });
+        }}
+      >
+        Press Me!
+      </button>
+      <p>{text}</p>
+    </div>
+  );
 };
 
 export default Translation;
