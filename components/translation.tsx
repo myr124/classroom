9630f038da90a16b110646552eabbd9734a2bad4
Eric George <ericgeorge@Erics-Air.attlocal.net>
2023-11-22 16:38:14 -0500

    implemented live transcription

--- components/translation.tsx
+++ components/translation.tsx
@@ -1,55 +1,60 @@
 import React, { useState } from "react";
 import * as mcsSDK from "microsoft-cognitiveservices-speech-sdk";
 const speechConfig = mcsSDK.SpeechConfig.fromSubscription(
-  process.env.NEXT_PUBLIC_SPEECH_KEY,
-  process.env.NEXT_PUBLIC_SPEECH_REGION
+  process.env.NEXT_PUBLIC_SPEECH_KEY ?? "",
+  process.env.NEXT_PUBLIC_SPEECH_REGION ?? ""
 );
 
-speechConfig.speechRecognitionLanguage = "hi-IN";
+speechConfig.speechRecognitionLanguage = "en-US";
 
 const Translation = () => {
   const [text, setText] = useState("test");
-  const [micOn, setMicOn] = useState(false);
+
+  const audioConfig = mcsSDK.AudioConfig.fromDefaultMicrophoneInput();
+  const speechRecognizer = new mcsSDK.SpeechRecognizer(
+    speechConfig,
+    audioConfig
+  );
+  const transcriber = new mcsSDK.ConversationTranscriber(
+    speechConfig,
+    audioConfig
+  );
+
+  transcriber.sessionStarted = (s, e) => {
+    console.log(e);
+  };
+  transcriber.sessionStopped = (s, e) => {
+    console.log(e);
+  };
+  transcriber.transcribing = (s, e) => {
+    console.log(e);
+  };
+
   return (
     <div className="flex flex-col justify-center align-middle">
       <button
         className="btn btn-primary "
         onClick={() => {
-          const audioConfig = mcsSDK.AudioConfig.fromDefaultMicrophoneInput();
-          const speechRecognizer = new mcsSDK.SpeechRecognizer(
-            speechConfig,
-            audioConfig
-          );
-          speechRecognizer.recognizeOnceAsync(async (result) => {
-            console.log("Starting");
-            switch (result.reason) {
-              case mcsSDK.ResultReason.RecognizedSpeech:
-                console.log(`RECOGNIZED: Text=${result.text}`);
-                setText(result.text);
-                break;
-              case mcsSDK.ResultReason.NoMatch:
-                console.log("NOMATCH: Speech could not be recognized.");
-                break;
-              case mcsSDK.ResultReason.Canceled:
-                const cancellation =
-                  mcsSDK.CancellationDetails.fromResult(result);
-                console.log(`CANCELED: Reason=${cancellation.reason}`);
-                if (cancellation.reason == mcsSDK.CancellationReason.Error) {
-                  console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
-                  console.log(
-                    `CANCELED: ErrorDetails=${cancellation.errorDetails}`
-                  );
-                  console.log(
-                    "CANCELED: Did you set the speech resource key and region values?"
-                  );
-                }
-                break;
-            }
-            speechRecognizer.close();
+          console.log("Starting");
+          transcriber.startTranscribingAsync(() => {
+            console.log("transcribing started");
+            transcriber.transcribed = (s, e) => {
+              setText(e.result.text);
+            };
           });
         }}
       >
-        Please press me!
+        Press Me!
+      </button>
+
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          console.log("stopping");
+          transcriber.stopTranscribingAsync(() => {});
+        }}
+      >
+        To stop press me!
       </button>
       <p className="p-4">{text}</p>
     </div>
