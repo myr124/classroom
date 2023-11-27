b29a89cfb4dc30c7a6cac29ed03f3a6b958d484e
Noor Newaz <noornewazinc@gmail.com>
2023-11-26 23:25:52 -0500

    import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Navbar from "@/components/navbar";
 import Funcswitch from "@/components/funcswitch";
-import Textarea from "@/components/textarea";
 
 export default function Home() {
   return (
