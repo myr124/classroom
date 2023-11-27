7c7b2977bcba352f0215141981d16e91857f8df6
Noor Newaz <noornewazinc@gmail.com>
2023-11-26 23:14:27 -0500

    Fix import error

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 import Navbar from "@/components/navbar";
 import Funcswitch from "@/components/funcswitch";
-import { ThemeProvider } from "next-themes";
 
 export default function Home() {
   return (
-    <ThemeProvider>
-      <div>
-        <Navbar />
-        <Funcswitch />
-      </div>
-    </ThemeProvider>
+    <div>
+      <Navbar />
+      <Funcswitch />
+    </div>
   );
 }
